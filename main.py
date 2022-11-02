deac6d8ffe26e258f470cacbf55bc782c59a0d4f
Vahid <kingvm8@gmail.com>
2022-11-02 22:21:23 +0330

    add some routs

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ def index():
     return {"data": {"name": "Vahid"}}
 
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+@app.get("/blog/unpublished")
+def unpublished():
+    # return all unpublished blogs
+    return {"data": "all unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def about(id: int):
+    # fetch blog with id = id
+    return {"blog": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    # fetch coments for blog with id = id
+    return {"data": {"1", "2"}}
