859358ced519490e7d76810a936b2d422a4ac6db
Vahid <kingvm8@gmail.com>
2022-11-03 21:40:00 +0330

    Query Parameters and String Validations

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 from typing import Optional
 from enum import Enum
@@ -15,6 +15,11 @@ def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
         return {f"{limit} published blogs from db"}
     else:
         return {f"{limit} unpublished blogs from db"}
+
+
+@app.post("/blog")
+def create_blog(blog: BlogModel):
+    return {"data": f"blog created with title as '{blog.title}'"}
 
 
 @app.get("/blog/unpublished")
@@ -35,11 +40,6 @@ def comments(id):
     return {"data": {"1", "2"}}
 
 
-@app.post("/blog")
-def create_blog(blog: BlogModel):
-    return {"data": f"blog created with title as '{blog.title}'"}
-
-
 class ModelName(str, Enum):
     product = "blog"
     colection = "comments"
@@ -49,3 +49,17 @@ class ModelName(str, Enum):
 @app.get("/model/{model_name}")
 def get_model(model_name: ModelName):
     return {f"model_name is {model_name}"}
+
+
+@app.get("/items")
+def read_items(
+    q: list[str] = Query(
+        default=[],
+        max_length=10,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        alias="item-query",
+        deprecated=True,
+    )
+):
+    return {"q": q}
